85a0b3d37cc6a92662ccd68387085634e13375ce
Ubuntu <ubuntu@ip-172-31-31-164.ec2.internal>
2022-07-21 16:27:08 +0000

    Finished KNN:

---
+++ src/k_nn/mod.rs
@@ -0,0 +1,71 @@
+
+pub struct Instance {
+    points: Vec<f64>,
+    label: i32,
+}
+
+impl Instance {
+    pub fn new(p: Vec<f64>, l: i32) -> Self {
+        return Self {
+            points:p, 
+            label: l
+        }
+    }
+}
+
+pub struct KNN {
+    k: i32,
+    data: Vec<Instance>,
+}
+
+impl KNN {
+    pub fn new(k: i32) -> Self {
+        return Self {
+            k: k,
+            data: Vec::new(),
+        }
+    }
+
+    fn euclidean_distance(x: &Vec<f64>, y: &Vec<f64>) -> f64{
+        // Ensure vector length the same
+        if x.len() != y.len() {
+            return 0.0
+        }
+
+        // Sum 
+        let mut sum: f64 = 0.0;
+        for i in 0..x.len() {
+            sum += (y[i] - x[i]).powf(2.0);
+        }
+
+        return sum.powf(0.5)
+    }
+
+    pub fn set_data(&mut self, data: Vec<Instance>) {
+        self.data = data;
+    }
+
+    fn calculate_distances(&self, c: &Vec<f64>) -> Vec::<(i32, f64)> {
+        // TODO: Make sure self has actual data before calling this fn
+
+        let mut distances = Vec::new();
+
+        for i in 0..self.data.len() {
+            distances.push((self.data[i].label, Self::euclidean_distance(&self.data[i].points, c)));
+        }
+
+        distances.sort_by(|a,b| a.1.partial_cmp(&b.1).unwrap());
+        return distances
+    }
+
+    pub fn classify(&self, c: &Vec<f64>) -> f64 {
+        let distances = Self::calculate_distances(&self, &c);
+
+        let mut sum: f64 = 0.0;
+        for i in 0..self.k {
+            sum += distances[i as usize].0 as f64;
+        }
+
+        return sum / self.k as f64
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use k_nn::KNN;
+use k_nn::Instance;
+pub mod k_nn;
+
 fn main() {
-    println!("Hello, world!");
+    let mut test = KNN::new(3);
+    let data: Vec<Instance> = vec![
+                                    Instance::new(vec![1.5, 2.0], 1),
+                                    Instance::new(vec![3.0, 5.0], 2),
+                                    Instance::new(vec![1.3, 1.89], 1)
+                                ];
+    test.set_data(data);
+    println!("{}", test.classify(&vec![1.2, 1.78]));
 }
