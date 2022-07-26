423c5b327f4948219337929c7d55c7aedc7afd72
Kyle's rust ec2 <ubuntu@ip-172-31-31-164.ec2.internal>
2022-07-26 17:14:53 +0000

    Finished inst file format

--- src/loader/mod.rs
+++ src/loader/mod.rs
@@ -1,5 +1,63 @@
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::Path;
+
 use super::instance::Instance;
 
-pub fn load(file_name: String) -> Vec<Instance> {
-    return Vec::new();
+pub fn load_instances(file_name: String) -> Vec<Instance> {
+    // Create Vec<Instance> to be returned
+    let mut return_vector: Vec<Instance> = Vec::new();
+
+    // Create Path and Display
+    let path = Path::new(&file_name);
+    let display = path.display();
+
+    // Open file and handle errors
+    let mut file = match File::open(&path) {
+        Err(why) => panic!("couldn't open {}: {}", display, why),
+        Ok(file) => file,
+    };
+
+    // Read into String buffer
+    let mut loaded = String::new();
+    file.read_to_string(&mut loaded).unwrap();
+
+    // Split the string into Vector
+    let mut v: Vec<&str> = loaded.split(";").collect();
+
+    while v.len() > 0 {
+        let v_string = v.remove(0).to_string();
+        let v_split: Vec<&str> = v_string.split(":").collect();
+        let points_string = v_split[0].to_string();
+        let points_split: Vec<&str> = points_string.split(",").collect();
+
+        let mut points: Vec<f64> = Vec::new();
+        for j in 0..points_split.len() {
+            points.push(points_split[j].parse().unwrap())
+        }
+
+        return_vector.push(Instance::new(points, v_split[1].parse().unwrap()))
+    }
+
+    return return_vector;
+}
+
+pub fn write_instances(instances: &Vec<Instance>, file_name: String) {
+    // Build String to write
+    let mut to_write: String = String::new();
+
+    for i in 0..instances.len() {
+        for j in 0..instances[i].points.len() {
+            to_write += &instances[i].points[j].to_string();
+            if j < instances[i].points.len() - 1 { to_write += ","}
+        }
+        to_write += ":";
+        to_write += &instances[i].label.to_string();
+
+        if i < instances.len() - 1 { to_write += ";" }
+    }
+
+    let mut file = File::create(file_name).unwrap();
+
+    let _result_that_must_be_used = file.write_all(&to_write.as_bytes()).unwrap();
 }
--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use rand::Rng;
 
 use rust_ml::knn::KNN;
 use rust_ml::instance::Instance;
-use rust_ml::loader::load;
+use rust_ml::loader::{load_instances,write_instances};
 
 fn main() {
     let mut test = KNN::new(3);
@@ -15,28 +15,18 @@ fn main() {
     for _i in 0..100000 {
         data.push(Instance::new(vec![rng.gen(), rng.gen()], rng.gen()))
     }
-
-    /* Specific data for correctness bench
-    data.push(Instance::new(vec![0.0, 1.0], 1));
-    data.push(Instance::new(vec![1.0, 1.0], 1));
-    data.push(Instance::new(vec![0.5, 1.5], 1));
-    data.push(Instance::new(vec![-4.0, -2.0], 2));
-    data.push(Instance::new(vec![-2.5, -2.8], 2));
-    data.push(Instance::new(vec![-4.0, -3.0], 2));
-    */
     
+    write_instances(&data, "test_random.inst".to_string());
 
     let c = vec![0.5, 0.5];
 
-    test.set_data(data);
+    test.set_data(load_instances("test_random.inst".to_string()));
 
     // Benchmark single thread
     let now = Instant::now();
-    println!("{}", test.classify(&c, 1));
-    println!("Single thread classified in {} milliseconds", now.elapsed().as_millis());
+    println!("Single thread classified as label {} in {} milliseconds", test.classify(&c, 1), now.elapsed().as_millis());
 
     // Benchmark multi thread
     let now = Instant::now();
-    println!("{}", test.classify(&c, 2));
-    println!("Two threads classified in {} milliseconds", now.elapsed().as_millis());
+    println!("Two threads classified as label {} in {} milliseconds", test.classify(&c, 2), now.elapsed().as_millis());
 }
